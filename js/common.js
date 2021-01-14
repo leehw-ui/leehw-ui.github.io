052f13501fde34d3c171bd5b1fc697c95b8a268d
leehw-ui <hw6395@gmail.com>
2021-01-14 22:52:34 +0900

    포트폴리오수정

--- js/common.js
+++ js/common.js
@@ -1,6 +1,20 @@
 $(document).ready(function (){
-	var site_cnt =$('.site_list li').length;
+	
+	//프로젝트 갯수
+	var site_cnt =$('.site_list li').length; 
 	$('.history_box .count').text(site_cnt);
+	
+	
+	// 헤더 fixed 효과, 소개 등장 효과 
+    $(window).scroll(function(){
+		$('.site_list li').each( function(i){
+            var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+            var bottom_of_window = $(window).scrollTop() + $(window).height();
+            if( bottom_of_window > bottom_of_object/2 ){
+                $(this).addClass('show');
+            }
+        }); 
+    });
 	
 });
 
