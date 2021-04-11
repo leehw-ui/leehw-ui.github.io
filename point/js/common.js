d1d3a6e9d0b1e0c1c2b021ce7939c91fe7754604
leehyewon <leehyewon@yuja.kr>
2021-04-11 20:56:29 +0900

    21/04/11 이혜원 포인트 모바일 test

---
+++ point/js/common.js
@@ -0,0 +1,61 @@
+$(document).ready(function(){
+	/*======== web :: start============*/
+	$('header .gnb_cont .gnb_menu_item').hover(function() {
+	  $(this).find('.sub_menu_wrap').stop().fadeIn();
+	}, function(){
+	  $(this).find('.sub_menu_wrap').hide();
+	});
+	/*======== mobile :: start ==========*/
+	$('header .btn_hamburger').click(function(){
+		$('.mobile_nav_wrap').addClass('opened');
+	});
+	
+	$('.mobile_nav_wrap .gnb_menu_item a').click(function(){
+		$('.mobile_nav_body .sub_menu').slideUp();
+		if(!$(this).parent().hasClass('on')){
+			$(this).siblings('ul').slideDown();
+			$(this).parent().addClass('on').siblings().removeClass('on');
+		}else{
+			$(this).siblings('ul').slideUp();
+			$(this).parent().removeClass('on');
+		}
+	});
+	
+	$('.mobile_nav_wrap .btn_close_menu').click(function(){
+		$('.mobile_nav_wrap').removeClass('opened');
+	});
+	
+	$('footer .footer_sitemap .footer_sitemap_menu a').click(function(){
+		$(this).siblings('ul').slideToggle();
+		$(this).parent().toggleClass('on');
+	});
+	/*======== mobile :: end ==========*/
+	/* scroll */
+    $(window).on('scroll', function(){
+        onScroll();
+    });
+	
+	function onScroll() {
+        var scrollTop = $(window).scrollTop();
+        var windowHeight = $(window).innerHeight();
+		
+		var animOffsetHeight = windowHeight * .75;
+		
+		$('.anim_group .anim').each(function(){
+			var offsetTop = $(this).offset().top;
+			if (  scrollTop + animOffsetHeight > offsetTop ) {
+				$(this).addClass('in');
+			}
+		});
+		
+		if(scrollTop>100){
+			$('header').addClass('fixed');
+		}
+	};
+	
+	/*start*/
+    $(window).on('load', function() {
+       onScroll();
+    });
+
+});
---
+++ point/js/main.js
@@ -0,0 +1,43 @@
+$(document).ready(function(){
+	var visu_swiper = new Swiper('.main_visual_wrap .swiper-container', {
+		pagination: {
+			el: '.swiper-pagination',
+		}
+	});
+	var step_swiper = new Swiper('.step_wrap .swiper-container', {
+		slidesPerView: 4, 
+		touchRatio:0,
+		breakpoints: { // 화면의 넓이가 320px 이상일 때 
+			766: { 
+				slidesPerView: 1.8,
+				touchRatio:1,
+				spaceBetween: 0,
+				centeredSlides:true
+			}
+		}
+    });
+//	step_swiper.destroy();
+	
+	var news_swiper = new Swiper('.news_wrap .swiper-container', {
+		slidesPerView: 3, 
+		touchRatio:0,
+		pagination: {
+			el: '.swiper-pagination',
+			type: 'progressbar',
+		},
+		breakpoints: { // 화면의 넓이가 320px 이상일 때 
+			768: { 
+				slidesPerView: 2.1,
+				touchRatio:1,
+				spaceBetween:0,
+			},
+			640: { 
+				slidesPerView: 1.5,
+				touchRatio:1,
+				spaceBetween:20,
+			}
+		}
+    });
+	
+
+});
